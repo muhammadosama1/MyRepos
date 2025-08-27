8c0026069192ce7367d305a4de4ce3aa8f38332e
muhammadosama1 <muhammad_osama123@hotmail.com>
2025-08-27 16:48:17 +0300

    Initial project setup

---
+++ MyRepos/MyReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReposApp.swift
+//  MyRepos
+//
+//  Created by Muhammad Osama on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyReposTests/MyReposTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyReposTests.swift
+//  MyReposTests
+//
+//  Created by Muhammad Osama on 27/08/2025.
+//
+
+import Testing
+
+struct MyReposTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
